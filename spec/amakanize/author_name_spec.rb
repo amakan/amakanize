fa9f645ac3e16b17f0e5f95520b08b7ba7ea1eb7
r7kamura <r7kamura@gmail.com>
2016-07-16 18:24:17 +0900

    Normaize and surround by parentheses enclosed alphanumerics in author name

---
+++ spec/amakanize/author_name_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe Amakanize::AuthorName do
+  let(:author_name) do
+    described_class.new(raw)
+  end
+
+  describe "#to_s" do
+    subject do
+      author_name.to_s
+    end
+
+    {
+      "ぽんかん⑧" => "ぽんかん(8)"
+    }.each do |key, value|
+      context "with #{key.inspect}" do
+        let(:raw) do
+          key
+        end
+
+        it { is_expected.to eq value }
+      end
+    end
+  end
+end
