84edb55a7cf864829c38c303c2a5751f2e3f7783
r7kamura <r7kamura@gmail.com>
2016-07-22 20:07:00 +0900

    Remove comments

--- lib/amakanize/series_name.rb
+++ lib/amakanize/series_name.rb
@@ -19,17 +19,6 @@ module Amakanize
     end
 
     # @note Override
-    # def to_s
-    #   @raw
-    #     .gsub(/[[:space:]]*[\(（〈《【「『【［\[〔｛\{«‹〘〚].*/, "")
-    #     .gsub(/[[:space:]]*通常版/, "")
-    #     .gsub(/[[:space:]]+[１-９\d①②③④⑤⑥⑦⑧⑨⑩].*/, "")
-    #     .gsub(/[[:space:]]*[１-９\d①②③④⑤⑥⑦⑧⑨⑩]+巻?\z/, "")
-    #     .gsub(/[[:space:]]*~.+~/, "")
-    #     .gsub(/\A[[:space:]]+|[[:space:]]+\z/, "")
-    # end
-
-    # @note Override
     def to_s
       self.class.filters.inject(@raw) do |result, filter|
         filter.call(result)
