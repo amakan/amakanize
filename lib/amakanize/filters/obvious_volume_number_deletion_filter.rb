c1270d86fc55ce352344c21a7a2e3b77df4c19b0
r7kamura <r7kamura@gmail.com>
2016-08-18 20:17:23 +0900

    Revert obvious volume detection

--- lib/amakanize/filters/obvious_volume_number_deletion_filter.rb
+++ lib/amakanize/filters/obvious_volume_number_deletion_filter.rb
@@ -5,7 +5,7 @@ module Amakanize
       # @param string [String] e.g. `"刀語 第十一話 毒刀・鍍"`
       # @return [String] e.g. `"刀語"`
       def call(string)
-        string.gsub(/\s*第#{Amakanize::PATTERN_OF_NUMERIC_CHARACTER}+(?:話|巻).*/, "")
+        string.gsub(/\s*第?#{Amakanize::PATTERN_OF_NUMERIC_CHARACTER}+(?:話|巻).*/, "")
       end
     end
   end
