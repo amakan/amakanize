3d62e58f091e560e939bd0a96a9040b00ae2499f
r7kamura <r7kamura@gmail.com>
2016-08-18 20:19:19 +0900

    Improve volume detection for アニウッド大通り

--- lib/amakanize/filters/obvious_volume_number_deletion_filter.rb
+++ lib/amakanize/filters/obvious_volume_number_deletion_filter.rb
@@ -2,10 +2,11 @@ module Amakanize
   module Filters
     class ObviousVolumeNumberDeletionFilter < BaseFilter
       # @note Override
-      # @param string [String] e.g. `"刀語 第十一話 毒刀・鍍"`
-      # @return [String] e.g. `"刀語"`
+      # @param string [String] e.g. `"刀語 第十一話 毒刀・鍍"`, `"アニウッド大通り 1: アニメ監督一家物語"`
+      # @return [String] e.g. `"刀語"`, `"アニウッド大通り"`
       def call(string)
         string.gsub(/\s*第?#{Amakanize::PATTERN_OF_NUMERIC_CHARACTER}+(?:話|巻).*/, "")
+          .gsub(/\s+第?#{Amakanize::PATTERN_OF_NUMERIC_CHARACTER}+(?:話|巻)?:\s+.*/, "")
       end
     end
   end
