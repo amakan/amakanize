c534d41035a139149a3d9bf212d71886fc55e1d4
r7kamura <r7kamura@gmail.com>
2016-08-22 20:34:24 +0900

    Add spaces around brackets except for ending

--- lib/amakanize/filters/brackets_normalization_filter.rb
+++ lib/amakanize/filters/brackets_normalization_filter.rb
@@ -26,10 +26,12 @@ module Amakanize
 
       # @note Override
       # @param string [String] e.g. `"IS〈インフィニット・ストラトス〉 1 (オーバーラップ文庫)"`
-      # @return [String] e.g. `"IS(インフィニット・ストラトス) 1 (オーバーラップ文庫)"`
+      # @return [String] e.g. `"IS (インフィニット・ストラトス) 1 (オーバーラップ文庫)"`
       def call(string)
         PAIRS.each_with_object(string) do |(open, close), result|
           result.gsub!(/#{open}(.+?)#{close}/, '(\1)')
+        end.gsub(/\s*\((.+?)\)(?:\z|(\s*))/) do
+          " \(#{$1})#{' ' if $2}"
         end
       end
     end
