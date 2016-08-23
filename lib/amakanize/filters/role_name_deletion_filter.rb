a20ff36bd2515fe53846c7c93a2225ad50dc2132
r7kamura <r7kamura@gmail.com>
2016-08-23 13:38:12 +0900

    Support イラスト:XXX author name role

--- lib/amakanize/filters/role_name_deletion_filter.rb
+++ lib/amakanize/filters/role_name_deletion_filter.rb
@@ -2,13 +2,14 @@ module Amakanize
   module Filters
     class RoleNameDeletionFilter < BaseFilter
       ROLE_NAMES = %w(
+        イラスト
         原作
         原案
         漫画
       )
 
       # @note Override
-      # @param string [String] e.g. `"漫画:ハノカゲ"`
+      # @param string [String] e.g. `"漫画:ハノカゲ"`,  `"ハノカゲ:漫画"`
       # @return [String] e.g. `"ハノカゲ"`
       def call(string)
         string.gsub(%r<\A#{::Regexp.union(ROLE_NAMES)}[:/]>, "").gsub(%r<[:/]#{::Regexp.union(ROLE_NAMES)}\z>, "")
