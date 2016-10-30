03b5dff60bb8393fca3a1250756b9b4be76a19c0
r7kamura <r7kamura@gmail.com>
2016-10-31 00:50:52 +0900

    Fix wrong indent

--- lib/amakanize/filters/trailing_double_parentheses_singlization_filter.rb
+++ lib/amakanize/filters/trailing_double_parentheses_singlization_filter.rb
@@ -4,10 +4,10 @@ module Amakanize
       # @note Override
       # @param output [String] e.g. `"ヒナまつり 11 (ヒナまつり)  (ビームコミックス(ハルタ) )"`
       # @return [Hash] e.g. `"ヒナまつり 11 (ヒナまつり)  (ビームコミックスハルタ )"`
-    def call(context:, output:)
+      def call(context:, output:)
         {
-            context: context,
-            output: output.sub(/\([^\(^\)]+?\([^\(]+?\)[^\)^\(]+?\)\z/){ '(' + $&.gsub(/[\(\)]/,'') + ')' },
+          context: context,
+          output: output.sub(/\([^\(^\)]+?\([^\(]+?\)[^\)^\(]+?\)\z/){ '(' + $&.gsub(/[\(\)]/,'') + ')' },
         }
       end
     end
